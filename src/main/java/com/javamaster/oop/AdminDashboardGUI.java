966b09798c3aa129887c5f4530c734a6ebc2d68a
Ashborn11 <162258062+CjBatuigas@users.noreply.github.com>
2025-03-04 16:18:49 +0800

    push dw sabi ni zach

--- src/main/java/com/javamaster/oop/AdminDashboardGUI.java
+++ src/main/java/com/javamaster/oop/AdminDashboardGUI.java
@@ -1952,20 +1952,6 @@ public class AdminDashboardGUI extends javax.swing.JFrame {
          jTextFieldEditGrossSemiMonthly.setText(infoToUpdate[17]);
          jTextFieldEditHourlyRateAllowance.setText(infoToUpdate[18]);
          
-         String birthday = infoToUpdate[3];
-        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
-        Date birthdayDate;
-        try {
-            birthdayDate = sdf.parse(birthday);
-        } catch (ParseException e) {
-            // handle parsing error
-            JOptionPane.showMessageDialog(this, "Error parsing birthday: " + e.getMessage());
-            return;
-        }
-        jCalendarBirthday.setDate(birthdayDate);
-        jCalendarBirthday.repaint();
-         
-        //dito cj
          
     }//GEN-LAST:event_jTableDatabaseMouseClicked
 
